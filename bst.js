5145165773a8d1692aaa3af040d88fa33ffc5233
Nikolas Melgarejo <nikoclops@gmail.com>
2018-11-13 12:22:03 -0500

    Adds happiness

--- bst.js
+++ bst.js
@@ -37,4 +37,73 @@ class BinarySearchTree {
       return this.right.find(key);
     }
   }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      } else if (this == this.parent.right) {
+        this.parent.right = node;
+      }
+
+      if (node) {
+        node.parent = this.parent;
+      }
+    } else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      } else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMax() {
+    if (!this.right) {
+      return this;
+    }
+    return this.right._findMax();
+  }
+
+  remove(key) {
+    if (this.key === null) {
+      return null;
+    } else if (this.key === key) {
+      if (this.left === null && this.right === null) {
+        this._replaceWith(null);
+      } else if (this.left === null && this.right !== null) {
+        this._replaceWith(this.right);
+      } else if (this.right === null && this.left !== null) {
+        this._replaceWith(this.left);
+      } else {
+        let node = this.left._findMax();
+        this.key = node.key;
+        this.value = node.value;
+        node.remove(node.key);
+      }
+    }
+  }
 }
+
+function main() {
+  const BST = new BinarySearchTree();
+  BST.insert(3, 'dog');
+  BST.insert(1, 'cat');
+  BST.insert(4, 'bat');
+  BST.insert(6, 'gnat');
+  console.log(BST, 'line 100');
+  BST.insert(9, 'shat');
+  BST.insert(2, 'wombat');
+  BST.insert(5, 'rat');
+  BST.insert(7, 'bird');
+
+  console.log(BST);
+}
+
+main();
