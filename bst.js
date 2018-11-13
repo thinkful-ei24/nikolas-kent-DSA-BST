934855e4a327bf2e8817357e1aa7e2007cac17f1
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-13 11:29:14 -0500

    Insert and remove

---
+++ bst.js
@@ -0,0 +1,40 @@
+class BinarySearchTree {
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    } else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this);
+      } else {
+        this.left.insert(key, value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this);
+      } else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    if (this.key === null) {
+      return null;
+    } else if (this.key === key) {
+      return this.value;
+    } else if (key < this.key) {
+      return this.left.find(key);
+    } else {
+      return this.right.find(key);
+    }
+  }
+}
